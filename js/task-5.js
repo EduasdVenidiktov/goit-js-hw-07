640705b9220172f6838e4cd896e303bf19b9b667
EduardVenidiktov <edven7777777@gmail.com>
2024-01-25 17:21:17 +0200

    HW-7

--- js/task-5.js
+++ js/task-5.js
@@ -1,19 +1,3 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-// const bodyEl = document.body;
-// const bodyColor = document.querySelector(".color");
-// const changeColorButton = document.querySelector(".change-color");
-
-// changeColorButton.addEventListener("click", () => {
-//   const color = getRandomHexColor();
-//   bodyEl.style.backgroundColor = color;
-//   bodyColor.textContent = color;
-// });
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
